97df4d97033748e5c3897dcd33027cc5b67c0d0a
troopy28 - Maxime <troopy28@yahoo.fr>
2017-04-23 21:41:57 +0200

    troopy28's landing
    + Citizens dependency
    + SonarLint related modifications
    + ReplicatedPlayer class, that's going to be used to reproduce players with a NPC from the Citizens API.

--- src/main/java/org/kubithon/replicate/broking/impl/ReplicationListener.java
+++ src/main/java/org/kubithon/replicate/broking/impl/ReplicationListener.java
@@ -2,7 +2,6 @@ package org.kubithon.replicate.broking.impl;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import java.util.Base64;
 import net.minecraft.server.v1_9_R2.EnumProtocol;
 import net.minecraft.server.v1_9_R2.EnumProtocolDirection;
 import net.minecraft.server.v1_9_R2.Packet;
@@ -11,34 +10,38 @@ import org.apache.commons.lang.exception.ExceptionUtils;
 import org.kubithon.replicate.ReplicatePlugin;
 import org.kubithon.replicate.broking.MessageListener;
 
+import java.util.Base64;
+
 /**
  * Listens for replications.
  *
  * @author Oscar Davis
  * @since 1.0.0
  */
-public class ReplicationListener implements MessageListener
-{
+public class ReplicationListener implements MessageListener {
 
     @Override
-    public void patternReceive(String pattern, String topic, String message)
-    {
+    public void patternReceive(String pattern, String topic, String message) {
         byte[] bytes = Base64.getDecoder().decode(message);
         ByteBuf buf = null;
-        try
-        {
+        try {
             buf = Unpooled.wrappedBuffer(bytes);
             PacketDataSerializer serializer = new PacketDataSerializer(buf);
             Packet<?> packet = EnumProtocol.PLAY.a(EnumProtocolDirection.SERVERBOUND, serializer.g());
-            packet.a(serializer);
-        }
-        catch (Exception e)
-        {
+
+            /**
+             * Checking if the packet is null before calling "a". To please to sonar.
+             */
+            if (packet != null)
+                packet.a(serializer);
+            else
+                ReplicatePlugin.get().getLogger().severe("Could not read incoming packet");
+
+
+        } catch (Exception e) {
             ReplicatePlugin.get().getLogger().severe("Could not read incoming packet: " + ExceptionUtils
                     .getFullStackTrace(e));
-        }
-        finally
-        {
+        } finally {
             if (buf != null)
                 buf.release();
         }
